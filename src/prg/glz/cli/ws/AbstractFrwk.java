0335e16c674cf085862e99f67e8733d3621765b3
Andres Galaz <andres.galaz@gmail.com>
2017-07-30 12:34:19 -0300

    Fix SSL

--- src/prg/glz/cli/ws/AbstractFrwk.java
+++ src/prg/glz/cli/ws/AbstractFrwk.java
@@ -3,9 +3,17 @@ package prg.glz.cli.ws;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
@@ -19,6 +27,30 @@ public abstract class AbstractFrwk {
     private List<String> cookies;
 
     public AbstractFrwk(String cUrl) throws FrameworkException {
+        /* Start of Fix */
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+
+        } };
+
+        SSLContext sc;
+        try {
+            sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (Exception e) {
+            throw new FrameworkException(e);
+        }
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) { return true; }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        /* End of the fix*/
         this.setUrlServer( cUrl );
         // init();
     }

975aca4dd176b9ec0b014ead555d7d613fa96139
Andres Galaz <andres.galaz@gmail.com>
2019-05-11 20:17:52 -0400

    v4001b

--- src/prg/glz/cli/frm/FrmPrincipal.java
+++ src/prg/glz/cli/frm/FrmPrincipal.java
@@ -30,7 +30,7 @@ public class FrmPrincipal extends JFrame {
         try {
             init();
             this.setVisible( true );
-            this.setTitle( "Cliente Sicronización Framwork Web - Compustrom v4.1a" );
+            this.setTitle( "Cliente Sicronización Framwork Web - Compustrom v4.1b" );
         } catch (Exception e) {
             logger.error( "No se pudo inicializar pantalla principal", e );
         }
